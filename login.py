7dc7e4497c1b4e9ffd30372b28aa38c24637c395
SIGIRACE <chickang9@gmail.com>
2024-12-14 18:52:00 +0900

    feat: login 기능 구현

---
+++ login.py
@@ -0,0 +1,7 @@
+class Login:
+    def __init__(self, id, password):
+        self.id = id
+        self.password = password
+
+    def login(self):
+        print(f"Login with {self.id} and {self.password}")
